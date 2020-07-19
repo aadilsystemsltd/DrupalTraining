ba05217143505d91ba51b5c10d2cabdae22240d5
Adil Raza Khan <adil.raza@systemsltd.com>
2020-07-19 23:59:05 +0500

    Commiting First Stage of Crud Operation.

--- web/sites/all/modules/custom/events/src/Form/eventForm.php
+++ web/sites/all/modules/custom/events/src/Form/eventForm.php
@@ -9,9 +9,17 @@ namespace Drupal\events\Form;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Messenger\MessengerTrait;
 
 class EventForm extends FormBase
 {
+  use MessengerTrait;
+
+  public function __construct()
+  {
+
+  }
+
 
   public function getFormId()
   {
@@ -23,6 +31,7 @@ class EventForm extends FormBase
     $form['Event_Title'] = array(
       '#type' => 'textfield',
       '#title' => ('Title'),
+      '#required' => TRUE,
     );
 
     $form['Event_Participants'] = array(
@@ -36,18 +45,24 @@ class EventForm extends FormBase
       '#title' => ('Image'),
       '#required' => TRUE,
       '#description' => ('Allowed extensions: gif png jpg jpeg'),
+      '#upload_location' => 'public://pictures/',
       '#upload_validators' => array(
         'file_validate_extensions' => array('gif png jpg jpeg'),
         // Pass the maximum file size in bytes
         'file_validate_size' => array(1 * 1024 * 1024),
-      ),
-      '#upload_location' => 'public://society/'
+      )
     );
 
     $form['Event_Gallery'] = array(
-      '#type' => 'number',
-      '#title' => ('Max Event Participants:'),
+      '#type' => 'managed_file',
+      '#title' => ('Event Gallery:'),
+      '#multiple' => TRUE,
       '#required' => TRUE,
+      '#upload_location' => 'public://pictures/Gallery',
+      '#upload_validators' => array(
+        'file_validate_extensions' => array('png gif jpg jpeg'),
+        'file_validate_size' => array(25600000)
+      ),
     );
 
     $form['Event_Start_End_Date'] = array(
@@ -55,14 +70,11 @@ class EventForm extends FormBase
       '#title' => ('Start & End Date:'),
       '#required' => TRUE,
     );
+
     $form['Event_ Category'] = array(
       '#type' => 'select',
-      '#title' => ('Category:'),
-      '#options' => array(
-        'female' => ('Female'),
-        'male' => ('Male'),
-      ),
-      '#required' => TRUE,
+      '#title' => ('Category'),
+      '#options' => $this->returnTaxonomy('event_category'),
     );
 
     $form['actions']['#type'] = 'actions';
@@ -71,13 +83,22 @@ class EventForm extends FormBase
       '#value' => $this->t('Save'),
       '#button_type' => 'primary',
     );
-
     return $form;
+  }
+
+  private function returnTaxonomy(string $vocabulary_name)
+  {
+    $terms = \Drupal::service('entity_type.manager')->getStorage("taxonomy_term")->loadTree($vocabulary_name, $parent = 0, $max_depth = NULL, $load_entities = FALSE);
+    foreach ($terms as $item) {
+      $key = $item->tid;
+      $value = $item->name;
+      $dropdown_array[$key] = $value;
+    }
+    return $dropdown_array;
   }
 
   public function validateForm(array &$form, FormStateInterface $form_state)
   {
-
     if (strlen($form_state->getValue('Event_Title')) == null) {
       $form_state->setErrorByName('Event_Title', $this->t('Event Name is Required.'));
     }
@@ -86,8 +107,9 @@ class EventForm extends FormBase
 
   public function submitForm(array &$form, FormStateInterface $form_state)
   {
-    // drupal_set_message($this->t('@can_name ,Your application is being submitted!', array('@can_name' => $form_state->getValue('candidate_name'))));
     foreach ($form_state->getValues() as $key => $value) {
+      // drupal_set_message(); depreciated in drupal 8.5
+      $this->messenger()->addMessage($key . ': ' . $value);
     }
   }
 }
